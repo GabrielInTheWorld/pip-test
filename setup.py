c441f1f421a7a416b0831c74b15e55f397f8258d
GabrielMeyer <meyergabriel@live.de>
2020-08-25 13:52:48 +0200

    Init package

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read
+
+setuptools.setup(
+    name="piptest",
+    version="1.0.0",
+    author="GabrielInTheWorld",
+    author_email="meyergabriel@live.de",
+    description="Package to provide auth functionalities",
+    long_description=long_description,
+    url="https://github.com/GabrielInTheWorld/pip-test",
+    pacakages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
